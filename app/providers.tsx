05037e371f33b75d019978149e610bc3ded7a904
huantr87-hub <huantr87@gmail.com>
2026-05-26 01:10:10 +0700

    feat(ui): implement dark mode using next-themes

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,21 +4,24 @@ import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "next-themes";
 
 export default function AppProviders({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>
-        {children}
-        <Toaster 
-          position="top-center" 
-          toastOptions={{
-            className: "scale-90 sm:scale-100",
-          }} 
-        />
-      </QueryClientProvider>
-    </SessionProvider>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <Toaster 
+            position="top-center" 
+            toastOptions={{
+              className: "scale-90 sm:scale-100",
+            }} 
+          />
+        </QueryClientProvider>
+      </SessionProvider>
+    </ThemeProvider>
   );
 }
